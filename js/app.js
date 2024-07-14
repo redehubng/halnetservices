35ea34ceb459e2a5c4a3ee8b5fb643e3b7873720
unknown <johnvictordml@gmail.com>
2024-07-14 23:08:44 +0100

    reduced the space between the navbar and header so the CTA buttons can be visible on other larger screen

--- js/app.js
+++ js/app.js
@@ -7,3 +7,39 @@ document.addEventListener('DOMContentLoaded', function() {
         navigationList.classList.toggle('active');
     });
 });
+
+
+// Feature card styles
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function checkCardsInViewport() {
+    const firstCard = document.querySelector('.first_card');
+    const secondCard = document.querySelector('.second_card');
+    const thirdCard = document.querySelector('.third_card');
+    const fourthCard = document.querySelector('.fourth_card');
+
+    if (isInViewport(firstCard)) {
+        firstCard.classList.add('rotated');
+    }
+    if (isInViewport(secondCard)) {
+        secondCard.classList.add('rotated');
+    }
+    if (isInViewport(thirdCard)) {
+        thirdCard.classList.add('rotated');
+    }
+    if (isInViewport(fourthCard)) {
+        fourthCard.classList.add('rotated');
+    }
+}
+
+window.addEventListener('scroll', checkCardsInViewport);
+window.addEventListener('resize', checkCardsInViewport);
+document.addEventListener('DOMContentLoaded', checkCardsInViewport);
